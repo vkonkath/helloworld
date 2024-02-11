549d1ece2b47fab384e5600a4f532861efd0bdfd
Vijay-nets <vkonk@nets.eu>
2024-02-11 08:02:42 +0200

    Initial commit

---
+++ include/hello_world.hpp
@@ -0,0 +1,13 @@
+#ifndef HELLO_WORLD_HPP
+#define HELLO_WORLD_HPP
+
+#include <iostream>
+
+class HelloWorld {
+public:
+    void print() {
+        std::cout << "Hello, World!" << std::endl;
+    }
+};
+
+#endif // HELLO_WORLD_HPP
---
+++ test/test.h
@@ -0,0 +1,14 @@
+//
+// Created by Vijayaraju Konkathi on 10.2.2024.
+//
+
+#ifndef HELLOWORLD_TEST_H
+#define HELLOWORLD_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //HELLOWORLD_TEST_H
