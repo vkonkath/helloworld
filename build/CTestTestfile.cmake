1389e68f0f6ee718b10bbe35fb1bc17d7cceaead
vkonkath <47735900+vkonkath@users.noreply.github.com>
2024-02-12 12:43:56 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vkonk/CLionProjects/helloworld
-# Build directory: /Users/vkonk/CLionProjects/helloworld/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[HelloWorldTest]=] "/Users/vkonk/CLionProjects/helloworld/build/HelloWorldTest")
-set_tests_properties([=[HelloWorldTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/vkonk/CLionProjects/helloworld/CMakeLists.txt;32;add_test;/Users/vkonk/CLionProjects/helloworld/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
